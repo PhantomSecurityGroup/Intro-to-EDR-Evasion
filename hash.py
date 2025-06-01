65ca67fee698aa0ea781f619d2cc8eb6139b3a50
isartorr <51035510+isartorr@users.noreply.github.com>
2025-06-01 10:26:22 -0400

    Added hashing helper script

---
+++ hash.py
@@ -0,0 +1,28 @@
+def hash_a(data: bytes, initial_seed: int = 7):
+    hash_val = 0
+    index = 0
+    length = len(data)
+
+    while index != length:
+        hash_val += data[index]
+        hash_val &= 0xFFFFFFFF
+        hash_val += (hash_val << initial_seed) & 0xFFFFFFFF
+        hash_val &= 0xFFFFFFFF
+        hash_val ^= (hash_val >> 6)
+        hash_val &= 0xFFFFFFFF
+        index += 1
+
+    hash_val += (hash_val << 3) & 0xFFFFFFFF
+    hash_val &= 0xFFFFFFFF
+    hash_val ^= (hash_val >> 11)
+    hash_val &= 0xFFFFFFFF
+    hash_val += (hash_val << 15) & 0xFFFFFFFF
+    hash_val &= 0xFFFFFFFF
+
+    return hash_val
+
+if __name__ == "__main__":
+    input_str = "VirtualProtect"
+    input_bytes = input_str.encode('ascii') 
+    result = hash_a(input_bytes)
+    print(f"#define {input_str}_HASH 0x{result:08X}")
